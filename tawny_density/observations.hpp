7d2f5f9cc0bce5845cd6b67070cd190613738389
Maree Carroll <maree.carroll@gmail.com>
2026-02-12 21:38:28 +1100

    Tidy up

--- tawny_density/observations.hpp
+++ tawny_density/observations.hpp
@@ -23,9 +23,7 @@ using std::string;
 
 namespace observations {
 
-const char USER_AGENT[] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/58.0.3029.110 Safari/537.36";
+const char USER_AGENT[] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)";
 
 const char URL_BASE[] = "https://api.inaturalist.org/v1/observations";
 
